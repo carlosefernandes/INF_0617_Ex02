c985bd8e63e7caa777044b5273e4fa9ebee27824
Carlos Fernandes <carlosf@motorola.com>
2018-10-13 11:52:08 -0300

    Remove duplicated code

--- reduce_CARLOS.py
+++ reduce_CARLOS.py
@@ -3,12 +3,11 @@ import sys
 large_diff = 0
 result = ""
 for line in sys.stdin:
-	for line in sys.stdin:
-		diff, info = line.split(':')
-		diff = float(diff)
-		# limit the large difference to 15oC
-		if (diff > large_diff and diff < 15):
-			large_diff = diff
-			info = info.replace("\n","")
-			result = info+ ", " +str(diff)
+	diff, info = line.split(':')
+	diff = float(diff)
+	# limit the large difference to 15oC
+	if (diff > large_diff and diff < 15):
+		large_diff = diff
+		info = info.replace("\n","")
+		result = info+ ", " +str(diff)
 print(result)
